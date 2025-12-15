3de3cc092599d27d166411e52d20cbec62314e85
shira <shira18k@gmail.com>
2025-12-15 15:50:44 +0200

    fixed

---
+++ src/Ass3Game/Ass3Game.java
@@ -0,0 +1,10 @@
+package Ass3Game;
+
+public class Ass3Game {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.initialize();
+        game.run();
+    }
+}
